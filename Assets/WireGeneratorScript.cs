618691481655ed25875ab30ce84db4855515495e
Vincent Flyer (Vincent Chan) <35939457+VFlyer@users.noreply.github.com>
2024-01-04 22:28:55 -0800

    Actual Initial Commit

---
+++ Assets/WireGeneratorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SRND = System.Random;
+using Random = UnityEngine.Random;
+using WireGen = WireGenerator.MeshGenerator;
+
+public class WireGeneratorScript : MonoBehaviour {
+	public MeshFilter affectedWire;
+	public MeshFilter[] clonedWires;
+	// Use this for initialization
+	void Awake () {
+		var rng = new SRND(Random.Range(int.MinValue, int.MaxValue));
+		var points = WireGen.GenerateWire(rng);
+		var generatedMesh = WireGen.GenerateWireMesh(points, Color.white);
+		affectedWire.sharedMesh = generatedMesh;
+		foreach (var filter in clonedWires)
+			filter.sharedMesh = generatedMesh;
+	}
+}
